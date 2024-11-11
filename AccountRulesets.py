9a6168523a6c290e55202b915033b620bcd24a20
Hassan-Mustafa-23 <hmustafa@adapture.com>
2024-11-11 07:28:02 -0500

    these scripts are added , with further testing more will be added as well

---
+++ AccountRulesets.py
@@ -0,0 +1,33 @@
+from cloudflarest.cloudfluser import AdaptureUser
+import httpx
+from pprint import pprint
+
+
+def get_account_rulesets(account_id, headers):
+    url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/rulesets"
+    response = httpx.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Failed to retrieve account rulesets for {account_id}: {response.status_code} - {response.text}")
+        return None
+    
+
+def main():
+    # Initialize the user with your token name
+    user = AdaptureUser('xx', token_name='token')
+    headers = user.credentials.headers
+
+    # Replace 'your_account_id' with the actual account ID you want to query
+    account_id = 'your_account_id'
+    
+    # Fetch the rulesets for the specified account ID
+    data = get_account_rulesets(account_id=account_id, headers=headers)
+    
+    # Print the retrieved data in a pretty format
+    pprint(data)
+
+# Entry point for the script
+if __name__ == "__main__":
+    main()
---
+++ ZoneWAFRules.py
@@ -0,0 +1,32 @@
+from cloudflarest.cloudfluser import AdaptureUser
+import httpx
+from pprint import pprint
+
+def get_zone_wafrules(zone_id, headers):
+    url = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/rulesets"
+    response = httpx.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Failed to retrieve zone rulesets for {zone_id}: {response.status_code} - {response.text}")
+        return None
+    
+
+def main():
+    # Initialize the user with your token name
+    user = AdaptureUser('GPC', token_name='token')
+    headers = user.credentials.headers
+
+    # Replace 'your_account_id' with the actual account ID you want to query
+    zone_id = '6edc93e749dfeced681d77c5027c4697'
+    
+    # Fetch the rulesets for the specified account ID
+    data = get_zone_wafrules(zone_id=zone_id, headers=headers)
+    
+    # Print the retrieved data in a pretty format
+    pprint(data)
+
+# Entry point for the script
+if __name__ == "__main__":
+    main()
